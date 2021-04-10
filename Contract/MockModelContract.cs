93a954b38feda6050ceff2e08d5e08a7481bf4b7
bs <bartosz.staniak@gmail.com>
2021-04-11 01:42:29 +0200

    added a mock for GetOneById method

--- Contract/MockModelContract.cs
+++ Contract/MockModelContract.cs
@@ -10,7 +10,7 @@ namespace API_exploration.Contract
     {
         public InitialModel GetOneById(int id)
         {
-            throw new NotImplementedException();
+            return new InitialModel { id = 0, Date = DateTime.Now, Location = "Tokio", RainChance = 90, Summary = "Stormy", TemperatureC = 30 };
         }
 
         public IEnumerable<InitialModel> GetWhateverItReturns()
