aab09af951b9df840e2fcb3ba2e49812c05382cb
bs <bartosz.staniak@gmail.com>
2021-04-10 18:09:07 +0200

    added a new value to the Summaries table

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace API_exploration.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Stormy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
