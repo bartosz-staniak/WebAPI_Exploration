2ed3bd986a7aa706b2056cb347ce0645cd28036f
bs <bartosz.staniak@gmail.com>
2021-04-11 00:57:05 +0200

    added a mock class that will implement the interface

---
+++ Contract/MockModelContract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_exploration.Contract
+{
+    public class MockModelContract : IModelContract
+    {
+    }
+}
