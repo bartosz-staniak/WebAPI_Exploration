3942151074f050a7413dbaf2f1da73f16dc36798
bs <bartosz.staniak@gmail.com>
2021-04-10 18:28:00 +0200

    added a Location field

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace API_exploration
     {
         public DateTime Date { get; set; }
 
+        public string Location { get; set; }
+
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
