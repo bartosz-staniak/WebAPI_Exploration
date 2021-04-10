de058fee9faeff4fc3a2814675b5200a398d00ef
bs <bartosz.staniak@gmail.com>
2021-04-10 22:29:57 +0200

    added an interface for InitialModel

--- Contract/IModelContract.cs
+++ Contract/IModelContract.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API_exploration.Models;
 
 namespace API_exploration.Contract
 {
     public class IModelContract
     {
+        IEnumerable<InitialModel> GetWhateverItReturns();
     }
 }
