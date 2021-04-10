807a337e98240f41ea06eeabb46c7948f04111a6
bs <bartosz.staniak@gmail.com>
2021-04-11 00:15:21 +0200

    added an argument the method will use

--- Contract/IModelContract.cs
+++ Contract/IModelContract.cs
@@ -9,6 +9,6 @@ namespace API_exploration.Contract
     public interface IModelContract
     {
         IEnumerable<InitialModel> GetWhateverItReturns();
-        InitialModel GetAllById();
+        InitialModel GetAllById(int id);
     }
 }
