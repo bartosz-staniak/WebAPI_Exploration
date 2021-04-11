75983810d7642baa025f1a565bc79c0fc75a9edc
bs <bartosz.staniak@gmail.com>
2021-04-11 18:53:34 +0200

    removed the logic and set some random value

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,10 +32,9 @@ namespace API_exploration.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            int subseqent = -1;
             return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
-                id = subseqent + 1,
+                id = rng.Next(0, 10000),
                 Date = DateTime.Now.AddDays(index),
                 Location = Cities[rng.Next(Cities.Length)],
                 TemperatureC = rng.Next(-20, 55),
