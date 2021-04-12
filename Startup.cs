67b1d47f029bb9357f9a9f0f8294156d338c86a4
bs <bartosz.staniak@gmail.com>
2021-04-12 02:08:17 +0200

    configured dependency injection for the repository

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using API_exploration.Contract;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace API_exploration
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<IModelContract, MockModelContract>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
