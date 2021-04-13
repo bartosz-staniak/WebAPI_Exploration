e5b67cee3b7908cfd67cb54ea12397ab9d2e7428
bs <bartosz.staniak@gmail.com>
2021-04-14 01:49:50 +0200

    using SqlServer

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace API_exploration
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<WhateverContext>;
+            services.AddDbContext<WhateverContext>(options => options.useSqlServer);
 
             services.AddControllers();
 
