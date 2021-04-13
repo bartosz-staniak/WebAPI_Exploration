2964d5a08d04d2d33805e1290bcb03e7f5b65010
bs <bartosz.staniak@gmail.com>
2021-04-14 01:37:12 +0200

    adding DbContext to use throughout the application

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,8 @@ namespace API_exploration
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext;
+
             services.AddControllers();
 
             services.AddScoped<IModelContract, MockModelContract>();
