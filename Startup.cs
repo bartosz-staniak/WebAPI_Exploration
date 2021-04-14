cae5ce602065a13e750bc44772f4c6333e8f6884
bs <bartosz.staniak@gmail.com>
2021-04-14 02:42:00 +0200

    getting configuration by ConnectionString

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace API_exploration
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<WhateverContext>(options => options
-            .useSqlServer(Configuration));
+            .useSqlServer(Configuration.GetConnectionString));
 
             services.AddControllers();
 
