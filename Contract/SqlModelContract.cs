d81a76ccc43a18fd36319279d3a40de7820e8f88
bs <bartosz.staniak@gmail.com>
2021-04-16 01:01:24 +0200

    implemented the methods of the interface

--- Contract/SqlModelContract.cs
+++ Contract/SqlModelContract.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using API_exploration.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace API_exploration.Contract
 {
     public class SqlModelContract : IModelContract
     {
+        public InitialModel GetOneById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<InitialModel> GetWhateverItReturns()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
