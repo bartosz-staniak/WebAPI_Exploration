e853b8753eee4b9628e46e1e9626c49ca504efe8
bs <bartosz.staniak@gmail.com>
2021-04-15 03:34:38 +0200

    added [Required] decorators to the remaining field

--- Models/InitialModel.cs
+++ Models/InitialModel.cs
@@ -10,14 +10,20 @@ namespace API_exploration.Models
     {
         [Key] // totally optional in this migration case
         public int id { get; set; }
+        
+        [Required]
         public DateTime Date { get; set; }
 
+        [Required]
         public string Location { get; set; }
 
+        [Required]
         public int TemperatureC { get; set; }
 
+        [Required]
         public int RainChance { get; set; }
 
+        [Required]
         public string Summary { get; set; }
     }
 }
