8575c0224a2a4edcbfa253a4344443ca2d062846
bs <bartosz.staniak@gmail.com>
2021-04-19 00:21:39 +0200

    created a separate file for the AnotherModel class

---
+++ Models/AnotherModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_exploration.Models
+{
+    public class AnotherModel
+    {
+        [Key]
+        public string Location { get; set; }
+
+        [Required]
+        public string Location_Size { get; set; }
+        [Required]
+        public string Population { get; set; }
+    }
+}
