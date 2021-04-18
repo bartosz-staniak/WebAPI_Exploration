416e4837833d806ea29c3e6dfe304f6d2ce970e4
bs <bartosz.staniak@gmail.com>
2021-04-19 01:51:26 +0200

    changed the route to GetWhateverItReturns

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace API_exploration.Controllers
         }
 
         // private readonly MockModelContract _modelContract = new MockModelContract();
-        [HttpGet("GetWhaterverItReturnsMocked")]
+        [HttpGet("GetWhaterverItReturns")] // used to be "GetWhaterverItReturnsMocked"
         public ActionResult <IEnumerable<WeatherForecast>> GetWhateverItReturns() // used to be GetHwateverItReturnsMocked
         {
             var whatevers = _modelContract.GetWhateverItReturns();
