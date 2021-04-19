5319ee2dd0994aa1b79ffeb2cb09c943c082dbc7
bs <bartosz.staniak@gmail.com>
2021-04-19 02:12:14 +0200

    new file with IAnotherContract interface

---
+++ Contract/IAnotherContract.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using API_exploration.Models;
+
+namespace API_exploration.Contract
+{
+    public interface IAnotherContract
+    {
+        IEnumerable<InitialModel> GetAllLocations();
+        InitialModel GetOneLocationByName(string location);
+    }
+}
