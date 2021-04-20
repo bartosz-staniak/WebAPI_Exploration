fbb2aa7169e35df8743605e5858873fb33627d7f
bs <bartosz.staniak@gmail.com>
2021-04-20 04:56:30 +0200

    removed the decorators

--- DTOs/WhateverReadDTO.cs
+++ DTOs/WhateverReadDTO.cs
@@ -7,25 +7,16 @@ namespace API_exploration.DTOs
 {
     public class WhateverReadDTO
     {
-        [Key] // totally optional in this migration case
         public int id { get; set; }
 
-        [Required]
         public DateTime Date { get; set; }
 
-        [Required]
         public string Location { get; set; }
-
-        [Required]
-        [MaxLength(3)]
+        
         public int TemperatureC { get; set; }
-
-        [Required]
-        [MaxLength(3)]
-        [Range(0, 100)]
+        
         public int RainChance { get; set; }
 
-        [Required]
         public string Summary { get; set; }
     }
 }
