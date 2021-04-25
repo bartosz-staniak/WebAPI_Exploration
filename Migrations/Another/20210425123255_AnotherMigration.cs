ffc2a239308cd03515a37c629f62a9667e4826c2
bs <bartosz.staniak@gmail.com>
2021-04-25 18:29:27 +0200

    added another table

---
+++ Migrations/Another/20210425123255_AnotherMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_exploration.Migrations.Another
+{
+    public partial class AnotherMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnotherModels",
+                columns: table => new
+                {
+                    Location = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Location_Size = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Population = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnotherModels", x => x.Location);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnotherModels");
+        }
+    }
+}
