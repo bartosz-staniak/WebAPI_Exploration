8b81ae0e34ee6928b8caaa1843c78f28bb50b1a6
bs <bartosz.staniak@gmail.com>
2021-04-25 18:31:59 +0200

    copied the default method from another controller

--- Controllers/AnotherController.cs
+++ Controllers/AnotherController.cs
@@ -10,5 +10,20 @@ namespace API_exploration.Controllers
     [Route("api/[controller]")]
     public class AnotherController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                id = rng.Next(0, 10000),
+                Date = DateTime.Now.AddDays(index),
+                Location = Cities[rng.Next(Cities.Length)],
+                TemperatureC = rng.Next(-20, 55),
+                RainChance = rng.Next(0, 90),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
