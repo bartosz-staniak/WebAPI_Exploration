c8770fbbb693b2793e103e0d5ea113448ade823e
bs <bartosz.staniak@gmail.com>
2021-04-25 18:35:37 +0200

    removed the contents from the method

--- Controllers/AnotherController.cs
+++ Controllers/AnotherController.cs
@@ -13,17 +13,7 @@ namespace API_exploration.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
-            {
-                id = rng.Next(0, 10000),
-                Date = DateTime.Now.AddDays(index),
-                Location = Cities[rng.Next(Cities.Length)],
-                TemperatureC = rng.Next(-20, 55),
-                RainChance = rng.Next(0, 90),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+        
         }
     }
 }
