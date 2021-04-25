dd49fab55e0cdbc48385a768714c3f28679355dd
bs <bartosz.staniak@gmail.com>
2021-04-25 22:23:13 +0200

    added implementation of the interface

--- Contract/SqlAnotherContract.cs
+++ Contract/SqlAnotherContract.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using API_exploration.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace API_exploration.Contract
 {
     public class SqlAnotherContract : IAnotherContract
     {
+        public IEnumerable<AnotherModel> GetAllLocations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnotherModel GetOneLocationByName(string location)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
