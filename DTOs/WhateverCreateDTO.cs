777f0daa59d47425674676748f2d2888d14fb014
bs <bartosz.staniak@gmail.com>
2021-04-26 04:24:16 +0200

    added all the fields from ReadDTO

--- DTOs/WhateverCreateDTO.cs
+++ DTOs/WhateverCreateDTO.cs
@@ -7,5 +7,18 @@ namespace API_exploration.DTOs
 {
     public class WhateverCreateDTO
     {
+        public int id { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public string Location { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int RainChance { get; set; }
+
+        public string Summary { get; set; }
+
+        // public string SubmittedBy { get; set; } // don't want to expose this field to API users
     }
 }
