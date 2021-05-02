e1c3a60db24a791d5d544f15edcb843809b4a10a
bs <bartosz.staniak@gmail.com>
2021-05-02 06:09:25 +0200

    reverted the last change

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -74,9 +74,6 @@ namespace API_exploration.Controllers
             } else if (whateverCreateDTO.Summary == "")
             {
                 return BadRequest(new { error = "Summary field must not be empty." });
-
-                [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$",
-                ErrorMessage = "Characters are not allowed.")]
             } else if (whateverCreateDTO.SubmittedBy == "")
             {
                 return BadRequest(new { error = "SubmittedBy field must not be empty." });
