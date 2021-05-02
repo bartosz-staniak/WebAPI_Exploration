4865ec220c127953e57ce674c0661b766447514b
bs <bartosz.staniak@gmail.com>
2021-05-02 06:06:10 +0200

    another silly attempt at using regex

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -55,6 +55,9 @@ namespace API_exploration.Controllers
         [HttpPost]
         public ActionResult<WhateverReadDTO> CreateWhatever(WhateverCreateDTO whateverCreateDTO)
         {
+
+            [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$",
+            ErrorMessage = "Characters are not allowed.")]
             if (whateverCreateDTO.TemperatureC > 60)
             {
                 return BadRequest(new { error = "The temperature cannot be higher than 60" });
