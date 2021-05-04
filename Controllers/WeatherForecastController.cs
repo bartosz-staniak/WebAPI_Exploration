494770a6162331a59338b6a9aa08cb6b2f77723e
bs <bartosz.staniak@gmail.com>
2021-05-04 03:33:39 +0200

    used the correct parameters this time

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -84,7 +84,7 @@ namespace API_exploration.Controllers
 
             var InitialModelReadDTO = _mapper.Map<WhateverReadDTO>(initialModel);
 
-            return CreatedAtRoute(nameof(GetOneById), new { id = whateverReadDTO.id }, whateverReadDTO); 
+            return CreatedAtRoute(nameof(GetOneById), new { id = InitialModelReadDTO.id }, InitialModelReadDTO); 
             // return Ok(InitialModelReadDTO); // used to be Ok(initialModel) but it also responded back with the SubmittedBy field
         }
 
