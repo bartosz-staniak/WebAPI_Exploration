50e2ac33eae94f1bc865c403e6503dd14035bd61
bs <bartosz.staniak@gmail.com>
2021-05-09 22:39:27 +0200

    copied class contents from CreateDTO

--- DTOs/WhateverUpdateDTO.cs
+++ DTOs/WhateverUpdateDTO.cs
@@ -7,18 +7,24 @@ namespace API_exploration.DTOs
 {
     public class WhateverUpdateDTO
     {
-        public int id { get; set; }
+        // public int id { get; set; } // the database will take care of assigning an id
 
+        [Required] // these Data Annotations make the response return a 400 Bad request code instead of a 500 server error code
         public DateTime DateAndTime { get; set; }
 
+        [Required]
         public string Location { get; set; }
 
         public int TemperatureC { get; set; }
 
+        [Range(0, 90)] // range validation works in this Create DTO context; it's also doubled in the controller
         public int RainChance { get; set; }
 
+        [Required]
+        [MaxLength(20)]
         public string Summary { get; set; }
 
-        // public string SubmittedBy { get; set; } // don't want to expose this field to API users
+        [Required]
+        public string SubmittedBy { get; set; }
     }
 }
