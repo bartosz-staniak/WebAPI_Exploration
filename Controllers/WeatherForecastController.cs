8baf71fb0254a31fb0a9fa897abc82ed1a4d21a8
bs <bartosz.staniak@gmail.com>
2021-05-19 03:47:57 +0200

    changed the name again

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -149,7 +149,7 @@ namespace API_exploration.Controllers
         [HttpDelete("{id}")]
         public ActionResult DeleteWhatever(int id)
         {
-            Boolean resourceFalse = true;
+            Boolean resourceDeleted = true;
 
             while (resourcePresent)
             {
