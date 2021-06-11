9a55ada289b660154d90b64353dc0d5db716549a
bs <bartosz.staniak@gmail.com>
2021-06-11 03:43:05 +0200

    added a new migration

---
+++ Migrations/20210611014241_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_exploration.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InitialModels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateAndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TemperatureC = table.Column<int>(type: "int", maxLength: 3, nullable: false),
+                    RainChance = table.Column<int>(type: "int", maxLength: 3, nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubmittedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InitialModels", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InitialModels");
+        }
+    }
+}
