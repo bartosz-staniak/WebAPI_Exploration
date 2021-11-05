f0063fb73f3ac560a3efe9ec8d2275e55300585c
bs <bartosz.staniak@gmail.com>
2021-11-05 17:35:07 +0100

    added SaveChanges method to save data in db

--- Contract/IAnotherModelContract.cs
+++ Contract/IAnotherModelContract.cs
@@ -5,6 +5,8 @@ namespace API_exploration.Contract
 {
     public interface IAnotherModelContract
     {
+        bool SaveChanges();
+
         IEnumerable<AnotherModel> GetAllLocations();
         AnotherModel GetOneLocationByName(string location);
     }
