9be5effb546d8fd527de43444db41465a58988ea
bs <bartosz.staniak@gmail.com>
2021-12-10 02:20:12 +0100

    added a new class

---
+++ Applications.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace API_exploration
+{
+    public class Applications
+    {
+        public string Company { get; set; }
+
+        public string Position { get; set; }
+
+        public int SalaryExpectations { get; set; }
+
+        public string Remarks { get; set; }
+
+        public string SubmittedBy { get; set; }
+    }
+}
