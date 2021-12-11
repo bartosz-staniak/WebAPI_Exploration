7cc5f7f0ad516193545e0a6b10f9bb190a1402d0
bs <bartosz.staniak@gmail.com>
2021-12-11 22:29:30 +0100

    added Applications Model

---
+++ Models/ApplicationsModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_exploration.Models
+{
+    public class ApplicationsModel
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Company { get; set; }
+
+        public string Position { get; set; }
+
+        public int SalaryExpectations { get; set; }
+
+        public string Remarks { get; set; }
+
+        public string SubmittedBy { get; set; }
+    }
+}
